2b6b4b2b2a7e6108fbd0d6f545eab2931ae9672a
Logan Taehwan Lee <leelogan93@gmail.com>
2023-06-16 21:16:39 +0000

    Clean up comments. Ref #1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}&include_adult=false&language=en-US&query=`;
 
-// App state
+// Application state
 const resultPage = 1;
 
 // Search button
